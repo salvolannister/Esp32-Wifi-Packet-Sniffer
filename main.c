d9f962b5c0507b5323e1a0429502fed15fdc0e36
salvolannister <salvatore.d.gurzi@gmail.com>
2018-12-04 23:04:26 +0100

    aggiunta struttura dati

--- main.c
+++ main.c
@@ -17,6 +17,7 @@
 #define	LED_GPIO_PIN			GPIO_NUM_4
 #define	WIFI_CHANNEL_MAX		(13)
 #define	WIFI_CHANNEL_SWITCH_INTERVAL	(500)
+#define NO_SSID "no ssid"
 
 static wifi_country_t wifi_country = {.cc="CN", .schan=1, .nchan=13, .policy=WIFI_COUNTRY_POLICY_AUTO};
 
@@ -35,9 +36,17 @@ typedef struct {
 	uint8_t payload[0]; /* network data ended with 4 bytes csum (CRC32) */
 } wifi_ieee80211_packet_t;
 
+typedef struct reduced_wifi_pkt_rx_ctrl_t{
+    signed rssi:8;
+    unsigned channel:4;
+    uint8_t mac_src[6];
+    char *ssid;
+    uint8_t length_ssid;
+}reduced_info;
+
 //dynamic data structure to contain sniffed packets
 typedef struct P_array{
-	wifi_ieee80211_packet_t* array; /*array of  packets*/
+	reduced_info* array; /*array of  packets*/
 	int count;    /* number of actual packet -> position is count-1 */
 	int dim; /* dimension of array */
 }P_array;
@@ -51,25 +60,26 @@ static void wifi_sniffer_packet_handler(void *buff, wifi_promiscuous_pkt_type_t 
 
 /*function to manage data storage*/
 P_array P_allocate(int dim);
-void P_push(P_array* sniffed_packet, wifi_ieee80211_packet_t x);
+void P_push(P_array* sniffed_packet, reduced_info x);
 void P_free(P_array* sniffed_packet);
 void P_resize(P_array* sniffed_packet);
+void P_printer(P_array sniffed_packet);
 
 P_array Sniffed_packet;
 
 void
 app_main(void)
 {
-	uint8_t level = 0, channel = 1;
-
+	uint8_t channel = 1;
+    Sniffed_packet=P_allocate(40);
 	/* setup */
 
 	wifi_sniffer_init();
-	gpio_set_direction(LED_GPIO_PIN, GPIO_MODE_OUTPUT);
+
 
 	/* loop */
 	while (true) {
-		gpio_set_level(LED_GPIO_PIN, level ^= 1);
+
 		vTaskDelay(WIFI_CHANNEL_SWITCH_INTERVAL / portTICK_PERIOD_MS);
 		wifi_sniffer_set_channel(channel);
 		channel = (channel % WIFI_CHANNEL_MAX) + 1;
@@ -80,7 +90,7 @@ P_array P_allocate(int dim){
 
     dim = dim+1;
 	P_array parray;
-	parray.array = (wifi_ieee80211_packet_t* )malloc(dim*sizeof(wifi_ieee80211_packet_t));
+	parray.array = (reduced_info* )malloc(dim*sizeof(reduced_info));
 	if(parray.array==NULL){
         printf("Not enough memory at initialating \n");
         exit(-1);
@@ -96,7 +106,8 @@ P_array P_allocate(int dim){
 void P_resize(P_array* sniffed_packet){
     int new_dim = sniffed_packet->dim*2;
     /* new_dim+= 40; */
-        sniffed_packet->array=(wifi_ieee80211_packet_t *) realloc(sniffed_packet->array,new_dim*sizeof(wifi_ieee80211_packet_t));
+    printf("Sto eseguendo la resize \n");
+        sniffed_packet->array=(reduced_info *) realloc(sniffed_packet->array,new_dim*sizeof(reduced_info));
         if(sniffed_packet->array == NULL){
             printf("No more space to realloc\n");
             exit(-1);
@@ -106,7 +117,7 @@ void P_resize(P_array* sniffed_packet){
 
 }
 
-void P_push(P_array* sniffed_packet, wifi_ieee80211_packet_t p){
+void P_push(P_array* sniffed_packet, reduced_info p){
   int n_packets = sniffed_packet->count;
 
   if((sniffed_packet->dim) <= n_packets)
@@ -167,6 +178,23 @@ wifi_sniffer_packet_type2str(wifi_promiscuous_pkt_type_t type)
 	}
 }
 
+void P_printer(P_array sniffed_packet){
+    reduced_info x;
+   int i=0;
+
+   for (i=0;  i< sniffed_packet.count; i++){
+    x = sniffed_packet.array[i];
+    printf("CHAN=%02d, RSSI=%02d ",x.channel,x.rssi);
+	if(x.length_ssid != 0 ){
+    printf(" SSID_length=%d SSID_%s",x.length_ssid,x.ssid);
+	}
+    printf(" MAC_SRC=%02x:%02x:%02x:%02x:%02x:%02x\n",
+        x.mac_src[0],x.mac_src[1],x.mac_src[2],
+		x.mac_src[3],x.mac_src[4],x.mac_src[5]);
+   }
+}
+
+
 void
 wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t type)
 {
@@ -177,11 +205,11 @@ wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t type)
 	const wifi_promiscuous_pkt_t *ppkt = (wifi_promiscuous_pkt_t *)buff;
 	const wifi_ieee80211_packet_t *ipkt = (wifi_ieee80211_packet_t *)ppkt->payload;
 	const wifi_ieee80211_mac_hdr_t *hdr = &ipkt->hdr;
+    reduced_info x;
 
 
 	uint16_t frame = hdr->frame_ctrl;
-	printf("frame = %x\n", frame);
-	printf("frame_ctrl = %x\n", hdr->frame_ctrl);
+
 
     uint16_t a, b=64, mask=0xF0;
     a= frame & mask;
@@ -190,7 +218,8 @@ wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t type)
 		return;
     }
 
-	char *ssid, stringa[32];
+
+	char *ssid;
 	uint8_t *data=&ppkt->payload;/*new*/
 	uint8_t len=data[25], i;
 	ssid=(char *) malloc((len+1)*sizeof(char));
@@ -198,26 +227,49 @@ wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t type)
 		ssid[i]=data[26+i];
 	}
 	ssid[i]='\0';
-	//ssid=strdup(stringa);
 
-	printf("PACKET TYPE=%s, CHAN=%02d, RSSI=%02d, TIME=%d, SSID=%d %s"
-		" ADDR1=%02x:%02x:%02x:%02x:%02x:%02x,"
-		" ADDR2=%02x:%02x:%02x:%02x:%02x:%02x,"
-		" ADDR3=%02x:%02x:%02x:%02x:%02x:%02x\n",
-		wifi_sniffer_packet_type2str(type),
-		ppkt->rx_ctrl.channel,
-		ppkt->rx_ctrl.rssi,
-		ppkt->rx_ctrl.timestamp,/*new*/
-		len, ssid,/*new*/
-		/* ADDR1 */
-		hdr->addr1[0],hdr->addr1[1],hdr->addr1[2],
-		hdr->addr1[3],hdr->addr1[4],hdr->addr1[5],
-		/* ADDR2 */
-		hdr->addr2[0],hdr->addr2[1],hdr->addr2[2],
-		hdr->addr2[3],hdr->addr2[4],hdr->addr2[5],
-		/* ADDR3 */
-		hdr->addr3[0],hdr->addr3[1],hdr->addr3[2],
-		hdr->addr3[3],hdr->addr3[4],hdr->addr3[5]
-	);
+	 /*potrebbero esserci dei problemi perché non sappiamo cos'è :8 */
+        x.channel=ppkt->rx_ctrl.channel;
+    x.length_ssid=len;
+    for(i = 0; i < 6 ; i ++)
+        x.mac_src[i] = hdr->addr2[i];
+    x.rssi = ppkt->rx_ctrl.rssi;
+    if(len!=0){
+      x.ssid=strdup(ssid);
+       if(x.ssid == NULL){
+        printf(" no more memory for SSID\n");
+        exit(-2);
+       }
+    }
+
+
+    /* only probe request are memorized */
+    P_push(&Sniffed_packet,x);
+//
+//	printf("PACKET TYPE=%s, CHAN=%02d, RSSI=%02d, TIME=%d, SSID=%d %s"
+//		" ADDR1=%02x:%02x:%02x:%02x:%02x:%02x,"
+//		" ADDR2=%02x:%02x:%02x:%02x:%02x:%02x,"
+//		" ADDR3=%02x:%02x:%02x:%02x:%02x:%02x\n",
+//		wifi_sniffer_packet_type2str(type),
+//		ppkt->rx_ctrl.channel,
+//		ppkt->rx_ctrl.rssi,
+//		ppkt->rx_ctrl.timestamp,/*new*/
+//		len, ssid,/*new*/
+//		/* ADDR1 */
+//		hdr->addr1[0],hdr->addr1[1],hdr->addr1[2],
+//		hdr->addr1[3],hdr->addr1[4],hdr->addr1[5],
+//		/* ADDR2 */
+//		hdr->addr2[0],hdr->addr2[1],hdr->addr2[2],
+//		hdr->addr2[3],hdr->addr2[4],hdr->addr2[5],
+//		/* ADDR3 */
+//		hdr->addr3[0],hdr->addr3[1],hdr->addr3[2],
+//		hdr->addr3[3],hdr->addr3[4],hdr->addr3[5]
+//	);
+      if(Sniffed_packet.count == 50){
+        P_printer(Sniffed_packet);
+        P_free(&Sniffed_packet);
+        exit(0);
+      }
+
 }
 
