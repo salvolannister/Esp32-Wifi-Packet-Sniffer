0273039de0f3d7109258b9fb73d9ba7cf9284a66
salvolannister <salvatore.d.gurzi@gmail.com>
2019-05-02 15:53:50 -0300

    scheletro GUI

---
+++ JavaFxGui/src/application/MainController.java
@@ -0,0 +1,10 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+public class MainController {
+
+		public void generateRandom(ActionEvent event) {
+			System.out.println("ciao ora puoi configurare");
+		}
+}
