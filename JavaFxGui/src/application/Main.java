1d784b966c82c6484f5faae7dc322bb3a4da1466
salvolannister <salvatore.d.gurzi@gmail.com>
2019-05-06 23:18:05 -0300

    Aggiunta scena configurazione

--- JavaFxGui/src/application/Main.java
+++ JavaFxGui/src/application/Main.java
@@ -13,8 +13,8 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 //			BorderPane root = new BorderPane();
-			Parent root = FXMLLoader.load(getClass().getResource("applicaton/Main.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- JavaFxGui/src/application/MainController.java
+++ JavaFxGui/src/application/MainController.java
@@ -1,10 +1,34 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class MainController {
-
-		public void generateRandom(ActionEvent event) {
-			System.out.println("ciao ora puoi configurare");
+//	it will tell that the controller is connected to FXML file 	
+	@FXML
+		
+		public void openConfiguration(ActionEvent event) {
+		Parent configurationPage;
+		try {
+			configurationPage = FXMLLoader.load(getClass().getResource("Configuration.fxml"));
+			Scene configurationPageScene = new Scene (configurationPage);
+			Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow(); 
+			appStage.hide();
+			appStage.setScene(configurationPageScene);
+			appStage.show();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			
+		}
+	
 		}
 }
