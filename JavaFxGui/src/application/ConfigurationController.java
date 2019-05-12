acb0a82623e356f3d9587428df2c0221a26e4767
salvolannister <salvatore.d.gurzi@gmail.com>
2019-05-11 23:22:45 -0300

    Aggiunti pannelli per MAC, posizione X e Y

--- JavaFxGui/src/application/ConfigurationController.java
+++ JavaFxGui/src/application/ConfigurationController.java
@@ -1,17 +1,23 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
 public class ConfigurationController implements Initializable{
@@ -21,9 +27,9 @@ public class ConfigurationController implements Initializable{
 	@FXML private Button OKButton;
 	@FXML private Spinner<Integer> SpinnerBox; 
 	
-	private float Height;
-	private float Width ;
-	private int nEsp;
+	private float Height = 0;
+	private float Width = 0 ;
+	private int nEsp = 0;
 	@FXML
 	private void setHeight(Event event) {
 		
@@ -47,13 +53,20 @@ public class ConfigurationController implements Initializable{
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		HeightBox.setText("0");
-//		SpinnerBox.setValueFactory(value);
 		HeightBox.setEditable(true);
 		SpinnerValueFactory<Integer> valueFactory = 
 	                new SpinnerValueFactory.IntegerSpinnerValueFactory(2,10,2);
-		SpinnerBox.setValueFactory(valueFactory);
 		
+		if(Height != 0 && Width != 0 && nEsp!= 0) {
+			String h = new String( Float.toString(Height));
+			String w = new String(Float.toString(Width));
+			HeightBox.setText(h);
+			WidthBox.setText(w);
+			valueFactory.setValue(nEsp);
+			SpinnerBox.setValueFactory(valueFactory);
+		}else {
+			SpinnerBox.setValueFactory(valueFactory);
+		}
 	}
 	
 	public void getSpinnerValue(Event event) {
@@ -61,6 +74,21 @@ public class ConfigurationController implements Initializable{
 	 System.out.println("nEsp "+nEsp);
 	}
 	
+	public void OKButtontEvent (Event event) {
+		Parent HomePage;
+		try {
+			HomePage = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene HomePageScene = new Scene (HomePage);
+			Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow(); 
+			appStage.hide();
+			appStage.setScene(HomePageScene);
+			appStage.show();
+			/*code to send information to the database*/
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			
+		}
 	}
-
+}
 
