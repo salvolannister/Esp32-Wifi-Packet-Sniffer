4af3a0f5dfab8b17983b328dfe90b4347bd642d3
ShadiSmith01 <uf95@live.it>
2019-05-25 18:16:27 +0200

    Aggiunto bottone "start"
    esegue il jar relativo al server

--- JavaFxGui/src/application/MainController.java
+++ JavaFxGui/src/application/MainController.java
@@ -1,8 +1,10 @@
 package application;
 
+import java.io.Console;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -30,5 +32,17 @@ public class MainController {
 			
 		}
 	
+		}
+
+		public void startServer(ActionEvent event){
+			System.out.println("cisono");
+			Runtime runTime = Runtime.getRuntime();
+			try {
+				Process process = runTime.exec("java -classpath /home/umb/Scrivania/PROGRAMMAZIONEDISISTEMA/progetto/MUDS/server/prova socket/out/production/Serverino:/home/umb/Scrivania/PROGRAMMAZIONEDISISTEMA/progetto/MUDS/server/prova socket/sqlite-jdbc-3.7.2.jar EchoServer");
+			//				Process process = Runtime.getRuntime().exec("java -jar /home/umb/Scrivania/PROGRAMMAZIONEDISISTEMA/progetto/MUDS/JavaFxGui/src/Serverino.jar");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 		}
 }
---
+++ JavaFxGui/src/module-info.java
@@ -0,0 +1,11 @@
+module JavaFxGui {
+
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.swing;
+    requires javafx.media;
+
+    opens application;
+}
