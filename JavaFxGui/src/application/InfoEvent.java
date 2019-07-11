6b0cb9220f0eac1b6eda8de4a2dd78aff921c6df
ShadiSmith01 <uf95@live.it>
2019-07-11 17:44:41 +0200

    Update vista "Show Mac's Frequency"
    -aggiunta info temporale

--- JavaFxGui/src/application/InfoEvent.java
+++ JavaFxGui/src/application/InfoEvent.java
@@ -8,19 +8,24 @@ import javafx.scene.input.MouseEvent;
 
 public class InfoEvent implements EventHandler<MouseEvent> {
     private String btn;
+    private String dataF;
+    private String dataI;
     private Long num;
     private TextArea AreaInfo;
 
-    public InfoEvent(Long cont, String name, TextArea area) {
+    public InfoEvent(Long cont, String name, TextArea area, String Idata, String Fdata) {
         this.btn=name;
         this.num=cont;
         this.AreaInfo=area;
+        this.dataF=Fdata;
+        this.dataI=Idata;
 
     }
 
     @Override
     public void handle(MouseEvent mouseEvent) {
         AreaInfo.setText("");
-        AreaInfo.appendText("il mac "+ btn+ "\n è stato rilevato "+num+ " volte");
+        AreaInfo.appendText("il mac "+ btn+ "\n è stato rilevato "+num+ " volte\n\n\n");
+        AreaInfo.appendText("Nell'intervallo "+dataI+"\na "+dataF);
     }
 }
