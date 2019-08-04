f33dd15932b70d2b0b356fd4ea8d08c01d4ca24b
salvolannister <salvatore.d.gurzi@gmail.com>
2019-08-04 19:53:02 -0300

    Query per leggere Configurazione
    Aggiunta query in QueryConf per poter leggere una configurazione conoscendone il nome.

--- JavaFxGui/src/DB/QueryConfiguration.java
+++ JavaFxGui/src/DB/QueryConfiguration.java
@@ -2,7 +2,11 @@ package DB;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class QueryConfiguration {
 
@@ -44,4 +48,39 @@ public class QueryConfiguration {
             return false;
         }
     }
+
+    public Map<String, ArrayList<Float>> readConfiguration(String name) {
+        PreparedStatement pstmt;
+        HashMap<String, ArrayList<Float>> risultato = new HashMap<>();
+
+        try {
+            conn.setAutoCommit(false);
+
+            String s = new String("SELCET MAC, X, Y FROM Configuration WHERE Name = ?");
+            try (PreparedStatement preparedStatement = pstmt = conn.prepareStatement(s)) {
+                pstmt.setString(1, name);
+                ResultSet res = pstmt.executeQuery();
+
+                while (res.next()) {
+                    ArrayList<Float> coordinates = new ArrayList<>();
+                    String MAC = res.getString("MAC");
+                    coordinates.add(res.getFloat("X"));
+                    coordinates.add(res.getFloat("Y"));
+                    System.out.println("MAC " + MAC + "X " + coordinates.get(0) + "Y " + coordinates.get(1));
+                }
+
+
+                if (risultato.isEmpty() == false) {
+                    conn.commit();
+                    return risultato;
+                }
+                return null;
+
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
 }
