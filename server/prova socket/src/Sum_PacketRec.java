828aadcd6d36e3095bbf468ef0fac64c5c78cf61
DaveBrain <cervella.davide@gmail.com>
2019-08-10 20:03:39 +0200

    Calcolo media RSSI per MAC catturati più volte

--- server/prova socket/src/Sum_PacketRec.java
+++ server/prova socket/src/Sum_PacketRec.java
@@ -1,19 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
 public class Sum_PacketRec {
 
+    //la prima volta che si rileva un mac da Tot schedine si inizializza RSSI
+    //quando si rileva in seguito dinuovo uno stesso mac si aggiunge all'entry iniziale
+    //dei valori di rssi nella lista RSSIs
     private Map<String, Integer> RSSI;
+    //todo debug
+    private ArrayList<Map<String, Integer>> RSSIs = new ArrayList<Map<String, Integer>>();
     private String MacSource;
     private String digest;
     private String TimeStamp;
 
-
     public Sum_PacketRec(Map<String, Integer> RSSI, String macSource, String digest, String timeStamp) {
+        //todo cancellare se funziona
         this.RSSI = RSSI;
+        //todo debug
+        Map<String, Integer> copyRssi = new HashMap<String,Integer>(RSSI);
+        this.RSSIs.add(copyRssi);
         MacSource = macSource;
         this.digest = digest;
         TimeStamp = timeStamp;
+    }
+    //todo debug
+    public void addRSSI(Map<String, Integer> rssi){
+        RSSIs.add(rssi);
+    }
+    //todo debug
+    public ArrayList<Map<String, Integer>> getRSSIs(){
+        return this.RSSIs;
     }
 
     public Map<String, Integer> getRSSI() {
